9fd4770a01990d2a1422ee692279ac102ac766a3
Script Raccoon <scriptraccoon@gmail.com>
2025-02-26 18:26:04 +0100

    small style improvements

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,15 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/youtube">YouTube</a>
+	<a href="https://github.com/ScriptRaccoon/youtube-recent-video-tutorial" target="_blank">Code</a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: center;
+		gap: 1.25rem;
+		padding-block: 1rem;
+		background-color: rgb(82, 22, 65);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,15 @@
 <script lang="ts">
 	import "./app.css"
+	import Nav from "$lib/Nav.svelte"
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/youtube">YouTube</a>
-	<a href="https://github.com/ScriptRaccoon/youtube-recent-video-tutorial" target="_blank">Code</a>
-</nav>
+<Nav />
 
 <div class="wrapper">
 	<slot></slot>
 </div>
 
 <style>
-	nav {
-		display: flex;
-		justify-content: center;
-		gap: 1.25rem;
-		padding-block: 1rem;
-		background-color: rgb(82, 22, 65);
-	}
-
 	.wrapper {
 		max-width: 42rem;
 		margin: 0 auto;
