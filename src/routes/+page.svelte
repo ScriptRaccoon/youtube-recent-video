84b78b6247ac99d5d457db3d5d690fc5f054e473
Script Raccoon <scriptraccoon@gmail.com>
2025-02-26 18:21:28 +0100

    clear hp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,4 @@
 
 <h1>Home</h1>
 
-<img src="https://voyager.postman.com/illustration/diagram-what-is-an-api-postman-illustration.svg" alt="API comic" />
-
-<div class="secondary">Image source: Postman</div>
+<p>Nothing here!</p>
